d5915d2dc12d820f0f29dab3917485a1ad8872c1
karanja03 <wambuik03@gmail.com>
2024-02-20 23:34:34 +0300

    Added gallery

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,82 +1,3 @@
-// import React from "react";
-
-
-// function AboutMe(){
-
-//     const aboutmestyle = {
-//         backgroundColor: '#12232B'
-//     };
-//     const cardstyles={
-//         backgroundColor: '#366A81'
-//     }
-    
-//     return(
-//         <div  id="about"  className=" text-center  "   style={aboutmestyle} >
-//                     <div className="pt-16">
-//                         <h2 className="animated-underline"> WHO'S THIS GUY?</h2>
-//                         <div className=" flex items-center justify-center ">
-//                         <div className="card w-3/5  mt-10 py-8 shadow-2xl" 
-//                         data-aos="zoom-out-down"
-//                         data-aos-duration="2000"
-//                         style={cardstyles}
-//                         >
-//                             <div class="card-body">
-//                         <h3 className="mt-4 text-white">  My Name is Collins Kimani</h3>
-//                         <p className="mt-4 font-medium text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/>
-//                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br/>
-//                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-//                         </div>
-//                         </div>
-                        
-//                 </div>
-
-//                 <div>
-//                     <h2 className="mt-20 animated-underline">WHAT DOES HE DO?</h2>
-//                     <div className="  flex items-center justify-center ">
-//                     <div className="card w-3/5  mt-10 py-8 shadow-2xl" 
-//                     data-aos="zoom-out-down"
-//                     data-aos-duration="2000"
-//                     style={cardstyles}
-
-//                     >
-//                     <div class="card-body">
-
-//                     <h3 className="mt-6 text-white"> I'm a Videographer</h3>
-//                     <p className="mt-4 font-medium text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/>
-//                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br/>
-//                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-//                     </div>
-//                     </div>
-                    
-
-//                         {/* <div className="  flex items-center justify-center "> */}
-
-//                     <div className=" card w-3/5 mt-10 py-8 shadow-2xl "  
-//                     data-aos="zoom-out-down"
-//                     data-aos-duration="2000"
-//                     style={cardstyles}
-
-//                     >
-//                         <div className="card-body">
-//                         <h3 className="mt-6 text-white ">I'm a Photographer</h3>
-//                     <p className="mt-4 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/>
-//                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br/>
-//                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-
-
-//                         </div>
-//                         {/* </div> */}
-
-//                     </div>
-//                     </div>
-            
-//                 </div>
-//                 </div>
-//         </div>
-//     )
-// }
-
-// export default AboutMe
 
 
 import React from "react";
@@ -92,7 +13,7 @@ function AboutMe() {
   };
 
   return (
-    <div id="about" className="text-center" style={aboutMeStyle}>
+    <div id="about" className="text-center pb-24" style={aboutMeStyle}>
       <div className="pt-16">
         <h2 className="animated-underline">WHO'S THIS GUY?</h2>
         <div className="flex items-center justify-center">
--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,11 +1,48 @@
-import React from "react";
 
-function Gallery(){
-    return(
-        <div id="gallery">
-            <h2 className="text-center">WELCOME TO MY GALLERY</h2>
 
-        </div>
-    )
-}
-export default Gallery
+
+import React, { useEffect, useState } from 'react';
+
+const DoggoGallery = () => {
+  const [galleryData, setGalleryData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/galleryImages'); // Adjust the port and host if needed
+        const data = await response.json();
+        setGalleryData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+
+    fetchData();
+  }, []);
+
+  const galleryStyles = {
+    backgroundColor: '#366A81',
+  };
+  const  imgContainerStyle={
+    backgroundColor:'#12232B'
+    
+  }
+  
+
+  return (
+    <div className='gallerybody ' style={galleryStyles}>
+      <h1 className='animated-underline mt-12 mb-14 '>MY GALLERY</h1>
+      <div className="grid-container">
+        {galleryData.map((image) => (
+          <div key={image.id} style={imgContainerStyle}>
+            <img className={`grid-item grid-item-${image.id}`} src={image.url} alt={`Image ${image.id}`} />
+            <p className='text-white'>{`"Caption for Image ${image.id}"`}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DoggoGallery;
